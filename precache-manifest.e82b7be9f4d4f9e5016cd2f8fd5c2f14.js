81b1283630143072b8179d0bdbac5561aa831d13
ruhci28 <59759735+ruhci28@users.noreply.github.com>
2020-08-23 20:09:29 +0530

    Updates

---
+++ precache-manifest.e82b7be9f4d4f9e5016cd2f8fd5c2f14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cf0f1c010b34b9afb6e156e8613f260",
+    "url": "/posts-react-redux/index.html"
+  },
+  {
+    "revision": "fbce1ec338cc69b40861",
+    "url": "/posts-react-redux/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "de38f85f9a94e6eae948",
+    "url": "/posts-react-redux/static/js/2.f92b74e7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/posts-react-redux/static/js/2.f92b74e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbce1ec338cc69b40861",
+    "url": "/posts-react-redux/static/js/main.3be126e4.chunk.js"
+  },
+  {
+    "revision": "7984b9c77727cf0573e2",
+    "url": "/posts-react-redux/static/js/runtime-main.3e044e95.js"
+  }
+]);
